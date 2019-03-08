2a4ab4589707963d535045cae00b36ed886eee53
Andreas Brommund <andreas@brommund.se>
2019-03-08 21:04:13 +0100

    Download HTML and retrieve the links

--- spindel.go
+++ spindel.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	url := "http://www.wallstedts.se"
+	//url = "http://www.brommund.se"
+	html := DownloadPage(url)
+
+	fmt.Printf("%s\n", html)
+
+	links := GetLinks(string(html))
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+}
+
+func DownloadPage(url string) string {
+	resp, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	html, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return string(html)
+}
+
+func GetLinks(content string) []string {
+	hrefRegexp := regexp.MustCompile(`(?mUis)href="(.*)"`)
+	links := make([]string, 0)
+
+	for _, link := range hrefRegexp.FindAllStringSubmatch(content, -1) {
+		links = append(links, link[1])
+	}
+
+	return links
 }
